0aea8a938a68fcd7c4373c2be627c4d8f480457d
gauhun <gsrsocial99@gmail.com>
2025-01-18 23:52:09 +0530

    deployment fixes

--- src/utils/imageLoader.ts
+++ src/utils/imageLoader.ts
@@ -1,5 +1,4 @@
 export default function imageLoader({ src }: { src: string }): string {
-    const basePath = process.env.NODE_ENV === 'production' ? '/garoono' : '';
     const cleanSrc = src.startsWith('/') ? src.slice(1) : src;
-    return `${basePath}/${cleanSrc}`;
+    return `/${cleanSrc}`;
 } 
