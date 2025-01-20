96d995e044332424f06c2b228e57526c9a1673de
gauhun <gsrsocial99@gmail.com>
2025-01-20 11:37:23 +0530

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-<<<<<<< HEAD
-    basePath: '',
+    basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
     images: {
         unoptimized: true,
         loader: 'custom',
         loaderFile: './src/utils/imageLoader.ts',
     },
-    assetPrefix: '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
     trailingSlash: true,
     distDir: 'out',
     webpack: (config) => {
         config.resolve.fallback = { fs: false };
         return config;
     },
-=======
-    basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-    images: {
-        unoptimized: true,
-    },
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
-    trailingSlash: true,
->>>>>>> 26bea40 (changes)
 };
 
 export default nextConfig; 
