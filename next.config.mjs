a21a6a0d4db30e92754ba889f4da9fb9ea96c950
gauhun <gsrsocial99@gmail.com>
2025-01-20 11:51:56 +0530

    tweak using nextjs github actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '',
-    assetPrefix: '',
     images: {
         unoptimized: true,
         loader: 'custom',
         loaderFile: './src/utils/imageLoader.ts',
     },
     trailingSlash: true,
-    distDir: 'out',
-    // Add this to ensure proper static file serving
-    webpack: (config) => {
-        config.resolve.fallback = { fs: false };
-        return config;
-    },
 };
 
 export default nextConfig; 
